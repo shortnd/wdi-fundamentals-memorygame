a0168913e6f28dfe026da9ffa6c6096b4d6e9ca2
Collin O'Connell <Shortnd@Collins-MacBook-Pro.local>
2017-01-02 07:46:38 -0600

    added if else to js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,20 @@
-console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardThree === cardFour){
+	alert("You found a match!")
+} else if (cardOne === cardTwo){
+	alert("You found a match!")
+} else if (cardTwo !== cardThree){
+	alert("Sorry, try again.")
+} else if (cardTwo !== cardFour){
+	alert("Sorry, try again.")
+} else if (cardOne !== cardThree){
+	alert("Sorry, try again.")
+} else if (cardOne !== cardFour){
+	alert("Sorry, try again.")
+} else {
+  //Do nothing
+}
