ef89aa2f06441d0f9227485899ee6473653ee2c7
Collin O'Connell <Shortnd@Collins-MacBook-Pro.local>
2017-01-08 13:15:59 -0600

    Organized Code and HTML chapter 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,18 +3,31 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardThree === cardFour){
-	alert("You found a match!")
-} else if (cardOne === cardTwo){
-	alert("You found a match!")
-} else if (cardTwo !== cardThree){
-	alert("Sorry, try again.")
-} else if (cardTwo !== cardFour){
-	alert("Sorry, try again.")
-} else if (cardOne !== cardThree){
-	alert("Sorry, try again.")
-} else if (cardOne !== cardFour){
-	alert("Sorry, try again.")
-} else {
-  //Do nothing
+//if (cardThree === cardFour){
+//	alert("You found a match!")
+//} else if (cardOne === cardTwo){
+//	alert("You found a match!")
+//} else if (cardTwo !== cardThree){
+//	alert("Sorry, try again.")
+//} else if (cardTwo !== cardFour){
+//	alert("Sorry, try again.")
+//} else if (cardOne !== cardThree){
+//	alert("Sorry, try again.")
+//} else if (cardOne !== cardFour){
+//	alert("Sorry, try again.")
+//} else {
+//  //Do nothing
+//}
+
+var board = document.getElementById('game-board')
+
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
+
+		board.appendChild(cardElement);
+	}
 }
