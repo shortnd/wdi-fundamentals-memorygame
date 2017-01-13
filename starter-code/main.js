4aea65a00af9c74c102cf6af83558197be0d5040
Collin OConnell <ococncol@gmail.com>
2017-01-12 19:42:53 -0500

    Updated index, main, and style to be able to run the game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,33 +1,51 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// represents the cards of the game
+var cards = ['queen','queen','king','king'];
+// represents the cards in play
+var cardsInPlay = [];
 
-//if (cardThree === cardFour){
-//	alert("You found a match!")
-//} else if (cardOne === cardTwo){
-//	alert("You found a match!")
-//} else if (cardTwo !== cardThree){
-//	alert("Sorry, try again.")
-//} else if (cardTwo !== cardFour){
-//	alert("Sorry, try again.")
-//} else if (cardOne !== cardThree){
-//	alert("Sorry, try again.")
-//} else if (cardOne !== cardFour){
-//	alert("Sorry, try again.")
-//} else {
-//  //Do nothing
-//}
+// find the board and set it to a variable
+//so that we can add cards to it
+var board = document.getElementById('game-board');
 
-var board = document.getElementById('game-board')
-
-function createBoard(){
-	for (var i = 0; i < cards.length; i++) {
-
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
 		var cardElement = document.createElement('div');
-
 		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 
-		board.appendChild(cardElement);
+
+		board.appendChild (cardElement);
+		board.appendChild (cardElement);
 	}
 }
+
+function isTwoCards() {
+
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king'){
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+//function reset() {
+//	document.getElementById('div')="";
+//}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+createBoard();
